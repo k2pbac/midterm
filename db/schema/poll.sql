41629f78e10570b305c7441eeec1370ad32eac9f
M <79332342+MM0nty@users.noreply.github.com>
2021-09-19 17:06:27 +0000

    Add polls and choices schemas and update users to include email

---
+++ db/schema/poll.sql
@@ -0,0 +1,13 @@
+-- Drop and recreate Polls table (Example)
+
+DROP TABLE IF EXISTS poll CASCADE;
+CREATE TABLE poll (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  creator_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  choice_id INTEGER REFERENCES choices(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
