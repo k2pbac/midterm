e6ed7cedfcde8ae68a20423a2c2cdacc308dce96
96sMicks <melkiasae@hotmail.com>
2021-09-20 15:12:12 +0000

    Updating the schema for users to run database against

--- db/seeds/01_users.sql
+++
@@ -1,5 +0,0 @@
--- Users table seeds here (Example)
-INSERT INTO users (name, email, shared_link, results_link)
-VALUES
-('Alice', 'alice@email.com'),
-('Kira', 'kira@email.com');
---
+++ db/seeds/01_users_seeds.sql
@@ -0,0 +1,7 @@
+-- Users table seeds here (Example)
+INSERT INTO users (name, email)
+VALUES
+('Alice', 'alice@email.com'),
+('Kira', 'kira@email.com'),
+('Michael', 'b@b.com'),
+('James', 'jamesa@gmail.com')
