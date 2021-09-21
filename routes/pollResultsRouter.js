c08382efe7b801450ea5c385ffde9373b6467f78
96sMicks <melkiasae@hotmail.com>
2021-09-21 18:38:19 +0000

    code clean up before pushing

--- routes/pollResultsRouter.js
+++ routes/pollResultsRouter.js
@@ -1,80 +1,60 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 const pollResultsRouter = (db) => {
-
   // Get results for a specific poll
-  router.get('/:poll_id', (req, res) => {
-    db.query(`
-  SELECT  options.option as name, SUM(point) as point_total, results.poll_id as poll, polls.title as question
-  FROM results
-  JOIN options ON option_id = options.id
-  JOIN polls ON results.poll_id = polls.id
-  WHERE results.poll_id = $1
-  GROUP BY options.option, results.poll_id, polls.title
-  ORDER BY poll, point_total DESC;`, [req.params.poll_id])
-    .then((response) => {
+  router.get("/:poll_id", (req, res) => {
+    db.query(
+      `
+    SELECT  options.option as name, SUM(point) as point_total, results.poll_id as poll, polls.title as question
+    FROM results
+    JOIN options ON option_id = options.id
+    JOIN polls ON results.poll_id = polls.id
+    WHERE results.poll_id = $1
+    GROUP BY options.option, results.poll_id, polls.title
+    ORDER BY poll, point_total DESC;`,
+      [req.params.poll_id]
+    )
+      .then((response) => {
+        // total used for dynamic sizing in bar graph display
+        let total = 0;
+        response.rows.forEach((element) => {
+          total += parseInt(element.point_total);
+        });
 
-      let total = 0;
-      response.rows.forEach(element => {
-        total += parseInt( element.point_total)
+        res.render("results", { total, polls: response.rows });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
-
-      console.log(total)
-      res.render("results", { total, polls: response.rows })
-
-
-    })
-    // (res.render("results"))
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
   });
 
   //  GET/ results/ all polls
-  router.get('/', (req, res) => {
-    console.log('---++')
-    db.query( `
+  router.get("/", (req, res) => {
+    console.log("---++");
+    db.query(
+      `
     SELECT  options.option as name, SUM(point) as point_total, results.poll_id as poll
     FROM results
     JOIN options ON option_id = options.id
     JOIN polls ON results.poll_id = polls.id
     GROUP BY options.option, results.poll_id
-    ORDER BY poll, point_total DESC;`)
-    .then((response) => {
-      let total = 0;
-      response.rows.forEach(element => {
-        total += parseInt( element.point_total)
+    ORDER BY poll, point_total DESC;`
+    )
+      .then((response) => {
+        let total = 0;
+        response.rows.forEach((element) => {
+          total += parseInt(element.point_total);
+        });
+        console.log("---++", total);
+        res.json({ total, data: response.rows });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
-      console.log('---++', total)
-      res.json({ total, data: response.rows });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-
-    // router.get('/', (req, res) => {
-    //   res.render("results")
-    // })
-
-
-  })
+  });
 
   return router;
-}
+};
 
-module.exports = pollResultsRouter
-
-//  How to render just the site */
-//  module.exports = (db) => {
-//    router.get("/", (req, res) => {
-
-//          res.render( "results");
-//        })
-
-//   return router;
-//  }
+module.exports = pollResultsRouter;
