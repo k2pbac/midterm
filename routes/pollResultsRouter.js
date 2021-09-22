7bf98771096b8cef489b93caec491483f23895ae
96sMicks <melkiasae@hotmail.com>
2021-09-22 19:16:48 +0000

    The query to get results raw data and the mock of results table under bar graph

--- routes/pollResultsRouter.js
+++ routes/pollResultsRouter.js
@@ -4,19 +4,22 @@ const router = express.Router();
 const pollResultsRouter = (db) => {
   // Get results for a specific poll
   router.get("/:poll_id", (req, res) => {
-    const promise1 = db.query(
-      `
+    const promise1 = db.query(`
     SELECT  options.option as name, SUM(point) as point_total, results.poll_id as poll, polls.title as question
     FROM results
     JOIN options ON option_id = options.id
     JOIN polls ON results.poll_id = polls.id
     WHERE results.poll_id = $1
     GROUP BY options.option, results.poll_id, polls.title
-    ORDER BY poll, point_total DESC;`,
-      [req.params.poll_id]
+    ORDER BY poll, point_total DESC;`, [req.params.poll_id]
     );
 
-    const promise2 = db.query(`SELECT * FROM results`);
+    const promise2 = db.query(`
+    SELECT voter_id AS users_id,  users.name as name, options.option AS choice, results.point AS point
+    FROM results
+    JOIN users ON voter_id = users.id
+    JOIN options ON option_id = options.id
+    WHERE results.poll_id = $1`, [req.params.poll_id]);
 
     Promise.all([promise1, promise2])
     .then((response) => {
@@ -28,8 +31,8 @@ const pollResultsRouter = (db) => {
           total += parseInt(element.point_total);
         });
         console.log(total)
-      // res.json({ data1: result1.rows , data2: result2.rows });
-      res.render("results", { total, polls: query1.rows, voteHistroy: query2.rows });
+      // res.json({ data1: query1.rows , data2: query2.rows });
+      res.render("results", { total, polls: query1.rows, voteHistory: query2.rows });
     })
     .catch((err) => {
       res.status(500).json({ error: err.message });
