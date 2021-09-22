832d8d83a824ea498f344ced97dcfa1e4aa7287b
M <79332342+MM0nty@users.noreply.github.com>
2021-09-22 19:26:56 +0000

    Add description fields to polls

--- public/scripts/create_poll.js
+++ public/scripts/create_poll.js
@@ -1,15 +1,15 @@
-$('document').ready(function () {
+$('document').ready(function() {
 
-  const createField = function () {
-    // <% for(let i=0; i< 8; i++) { %>
+  const createField = function() {
+    // <% for (let i = 0; i < 8; i++) { %>
     const field =
       `<li><input type="text" name="option${i + 1}" id="option${i + 1}" class="option" maxlength="255"></li>
-      <li><input name="description${i + 1}" type="text" id="description${i + 1}" class="description" maxlength="255"></li>`;
+      <li><input name="description${i + 1}" type="text" id="description${i + 1}" class="description" placeholder="Description" maxlength="255"></li>`;
     return field;
   }
 
   //creates as many options as wanted
-  $("#add_option").on('click', function (event) {
+  $("#add_option").on('click', function(event) {
     event.preventDefault();
     // renderFields(field);
     const $option = createField(field);
