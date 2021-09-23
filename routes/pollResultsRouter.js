fe0c810a60a99ffaf70f216c568cc318a44745a0
96sMicks <melkiasae@hotmail.com>
2021-09-23 03:44:46 +0000

    Result table now scrollable

--- routes/pollResultsRouter.js
+++ routes/pollResultsRouter.js
@@ -5,23 +5,23 @@ const pollResultsRouter = (db) => {
   // Get results for a specific poll
   router.get("/:poll_id", (req, res) => {
     const promise1 = db.query(`
-    SELECT  options.option as name, SUM(point) as point_total, results.poll_id as poll, polls.title as question
-    FROM results
-    JOIN options ON option_id = options.id
-    JOIN polls ON results.poll_id = polls.id
-    WHERE results.poll_id = $1
-    GROUP BY options.option, results.poll_id, polls.title
-    ORDER BY poll, point_total DESC;`, [req.params.poll_id]
+      SELECT  options.option as name, SUM(point) as point_total, results.poll_id as poll, polls.title as question
+      FROM results
+      JOIN options ON option_id = options.id
+      JOIN polls ON results.poll_id = polls.id
+      WHERE results.poll_id = $1
+      GROUP BY options.option, results.poll_id, polls.title
+      ORDER BY poll, point_total DESC;`, [req.params.poll_id]
     );
 
     const promise2 = db.query(`
-    SELECT voter_id AS users_id,  users.name as name, options.option AS choice, results.point AS point
-  FROM results
-  JOIN users ON voter_id = users.id
-  JOIN options ON option_id = options.id
-  WHERE results.poll_id = $1
-  GROUP BY users.name, voter_id, options.option, results.point
-  ORDER BY name, point DESC`, [req.params.poll_id]);
+      SELECT voter_id AS users_id,  users.name as name, options.option AS choice, results.point AS point
+      FROM results
+      JOIN users ON voter_id = users.id
+      JOIN options ON option_id = options.id
+      WHERE results.poll_id = $1
+      GROUP BY users.name, voter_id, options.option, results.point
+      ORDER BY name, point DESC`, [req.params.poll_id]);
 
     Promise.all([promise1, promise2])
     .then((response) => {
